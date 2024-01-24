cac08e89bed75c06974c1d365519cfd60fc5b75a
Lemasc Service <lemascserviceth@hotmail.com>
2024-01-24 20:25:26 +0700

    fix: oversized width in about page

--- src/Sections/Persernalinformation/index.jsx
+++ src/Sections/Persernalinformation/index.jsx
@@ -2,48 +2,48 @@ const Persernalinformation = () => {
     return(
         <div>
          
-          <div className='m-7 text-xl gap-5 grid grid-cols-[5fr_5fr]'>
+          <div className='m-7 text-xl gap-3 lg:gap-5 grid lg:grid-cols-[5fr_5fr]'>
             
-              <div className='grid gap-3 font-normal'>ชื่อ:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>นายธีระเศรษฐ์ พิมลวิรัชกุล(กิลเบิร์ต)</div>
+              <div className='font-medium'>ชื่อ:</div>
+              <div className='font-normal text-gray-600'>นายธีระเศรษฐ์ พิมลวิรัชกุล(กิลเบิร์ต)</div>
 
-              <div className='grid gap-3 font-normal'>ชั้น:</div>
-              <span className='grid gap-3 font-extralight text-gray-600'>ม.4/3</span>
+              <div className='font-medium'>ชั้น:</div>
+              <span className='font-normal text-gray-600'>ม.4/3</span>
               
-              <div className='grid gap-3 font-normal'>แผนการเรียน:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>ภาษาอังกฤษ-ภาษาญี่ปุ่น</div>
+              <div className='font-medium'>แผนการเรียน:</div>
+              <div className='font-normal text-gray-600'>ภาษาอังกฤษ-ภาษาญี่ปุ่น</div>
               
-              <div className='grid gap-3 font-normal'>อายุ:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>15</div>
+              <div className='font-medium'>อายุ:</div>
+              <div className='font-normal text-gray-600'>15</div>
               
-              <div className='grid gap-3 font-normal'>เกิดวันที่:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>23 กุมภาพันธ์ พ.ศ. 2551</div>
+              <div className='font-medium'>เกิดวันที่:</div>
+              <div className='font-normal text-gray-600'>23 กุมภาพันธ์ พ.ศ. 2551</div>
               
-              <div className='grid gap-3 font-normal'>หมู่เลือด:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>A</div>
+              <div className='font-medium'>หมู่เลือด:</div>
+              <div className='font-normal text-gray-600'>A</div>
               
-              <div className='grid gap-3 font-normal'>ส่วนสูง:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>178 ซม.</div>
+              <div className='font-medium'>ส่วนสูง:</div>
+              <div className='font-normal text-gray-600'>178 ซม.</div>
               
-              <div className='grid gap-3 font-normal'>น้ำหนัก:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>46 กก.</div>
+              <div className='font-medium'>น้ำหนัก:</div>
+              <div className='font-normal text-gray-600'>46 กก.</div>
               
-              <div className='grid gap-3 font-normal'>อาหารที่ชอบ:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>อาหารไทย,ฝรั่งเศษ,จีน,ญี่ปุ่น</div>
+              <div className='font-medium'>อาหารที่ชอบ:</div>
+              <div className='font-normal text-gray-600'>อาหารไทย,ฝรั่งเศษ,จีน,ญี่ปุ่น</div>
               
-              <div className='grid gap-3 font-normal'>ตัวละครที่ชอบ:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>Scarlet Witch , Ilumia</div>
+              <div className='font-medium'>ตัวละครที่ชอบ:</div>
+              <div className='font-normal text-gray-600'>Scarlet Witch , Ilumia</div>
               
-              <div className='grid gap-3 font-normal'>งานอดิเรก:</div>
-              <div className='grid gap-3 font-extralight text-gray-600'>แต่งกลอน,วาดรูป</div>
+              <div className='font-medium'>งานอดิเรก:</div>
+              <div className='font-normal text-gray-600'>แต่งกลอน,วาดรูป</div>
               
-              <div className='grid gap-3 font-normal'>Instagram พรรค:</div>
-              <a href="https://www.instagram.com/gilbertlnwza333.airline/" target='_blank' className='grid gap-3 font-extralight text-primary1 hover:text-orange-500'>
+              <div className='font-medium'>Instagram พรรค:</div>
+              <a href="https://www.instagram.com/gilbertlnwza333.airline/" target='_blank' className='font-normal text-primary1 hover:text-orange-500'>
                        @gilbertlnwza333.airline
                     </a>
               
-              <div className='grid gap-3 font-normal'>Instagram ส่วนตัว:</div>
-              <a href="https://www.instagram.com/scarlet_marrionante_/" target='_blank' className='grid gap-3 font-extralight text-primary1 hover:text-orange-500'>
+              <div className='font-medium'>Instagram ส่วนตัว:</div>
+              <a href="https://www.instagram.com/scarlet_marrionante_/" target='_blank' className='font-normal text-primary1 hover:text-orange-500'>
                        @scarlet_marrionante_
                    </a>
 
